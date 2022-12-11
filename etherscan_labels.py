776e548bf966613d7c3fa4bb7c032054a7bafcdd
brihu.sundararaman@yale.edu <brihu.sundararaman@yale.edu>
2022-12-11 11:59:12 -0500

    etherscan urls

---
+++ etherscan_labels.py
@@ -0,0 +1,23 @@
+# Labels from paper (slightly amplified)
+
+# DEX/Exchange Accounts - https://etherscan.io/accounts/label/dex
+# DEX/Exchange Tokens - https://etherscan.io/accounts/label/dex
+# Token-Contract - https://etherscan.io/tokens/label/deflationary-token
+# Tokenized-Asset-Contract - https://etherscan.io/tokens/label/tokenized-asset
+# Spam-Token-Accounts - https://etherscan.io/tokens/label/tokenized-asset
+# Spam-Token-Contracts - https://etherscan.io/tokens/label/spam-token
+# Gaming-Accounts - https://etherscan.io/accounts/label/gaming
+# Gaming-Token-Contracts - https://etherscan.io/tokens/label/gaming
+# Wallet-App-Accounts - https://etherscan.io/accounts/label/wallet-app 
+# Wallet-App-Token-Contracts - https://etherscan.io/tokens/label/wallet-app
+# Cold-Wallet-Accounts - https://etherscan.io/accounts/label/cold-wallet
+
+
+# Potential New Labels 
+# airdrop hunter accounts - https://etherscan.io/accounts/label/airdrop-hunter
+# alameda research accounts - https://etherscan.io/accounts/label/alameda-research
+# airdrop distributor - https://etherscan.io/accounts/label/airdrop-distributor
+
+
+
+
--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import random
 from extract_random_nodes import extract_random_nodes
 from overview import overview
 
+'''
+To get setup:
+
+Download the pkl file from https://www.kaggle.com/datasets/xblock/ethereum-phishing-transaction-network?resource=download
+(It is too large to upload to GitHub so to run the code locally, you will need to download it from Kaggle)
+
+'''
+
+# loading full dataset
 def load_pickle(fname):
     with open(fname, 'rb') as f:
         return pickle.load(f)
@@ -12,6 +21,9 @@ G = load_pickle('./MulDiGraph.pkl')
 print('Pickle file loaded.')
 print('Overview of the graph (first 10 nodes and edges')
 overview(G)
+
+# extracting random sample of nodes as done in paper
+
 print('Extracting random nodes...')
 G_subgraph = extract_random_nodes(G, 1165, 1165)
 print('Random nodes extracted.')
@@ -23,11 +35,17 @@ with open('./MulDiGraph_subgraph.pkl', 'wb') as f:
     pickle.dump(G_subgraph, f)
 print('Subgraph saved.')
 
+'''Replicating their experiment'''
+# TODO: Add Ethereum labels to the subgraph (see etherscan_labels.py) (Brihu)
+# TODO Amplify random nodes with neighbors nodes after labelling (Brihu)
+# TODO: Run Heterogeneous Graph Neural Network on the subgraph as shown in paper (Ez + Boris)
 
-# TODO: Choose Ethereum labels and get associated wallets/accounts
-# TODO: Add Ethereum labels to the subgraph (pick more than what they just did in the paper)
-# TODO: Get transaction hash for each edge --> save in CSV file
-# TODO: Get transaction value and other metadata for each edge in each edge --> save in CSV file and add to subgraph at runtime
-# TODO: Run GNN pipeline on the subgraph as shown in paper
+'''Additional work that we are doing'''
+# TODO: Get transaction hash for each edge --> save in CSV file (Brihu)
+# TODO: Get transaction value and other metadata for each edge in each edge --> save in CSV file and add to subgraph at runtime (Brihu)
+# TODO: Add any any extra labels that we want (see etherscan_labels.py) (Brihu)
+# TODO: Run Heterogeneous Graph Neural Network on the subgraph as shown in paper (Ez + Boris)
+
+'''Writeup'''
 
 print('Done.')
